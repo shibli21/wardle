de911d1180203ce4a5a82bbc8eec8e32eb9dc3ca
syed shibli mahmud <syedshiblimahmud@gmail.com>
2025-07-18 02:05:06 +0600

    Refactor import path for PhaserGame in App.tsx to use relative path

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useRef } from "react";
-import { IRefPhaserGame, PhaserGame } from "../../../PROJECTS/warrdle-clone-phaser/src/game/PhaserGame";
+import { IRefPhaserGame, PhaserGame } from "./game/PhaserGame";
 
 function App() {
     const phaserRef = useRef<IRefPhaserGame | null>(null);
